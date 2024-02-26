554af13324fbdcd416f6700666073d9953eb2083
Super Star <3339398358@qq.com>
2024-02-26 15:15:08 +0800

    init program

---
+++ FeishuCardMessage.py
@@ -0,0 +1,128 @@
+import json
+import requests
+import numpy as np
+from lib.log import LOGGER as logger
+
+
+class FeishuCardMessage(object):
+    def __init__(self):
+        self.data = {
+            "msg_type": "interactive",
+            "card": {"elements": []},
+        }
+
+    def message_send(self, webhook, proxy=""):
+        content = json.dumps(self.data, default=self.default_handler)
+        r = requests.post(url=webhook, data=content, proxies=proxy, verify=False)
+        logger.info("Sending to Feishu")
+        logger.debug("Feishu Message request result: %s", r.json())
+    
+    def md_add_color(self, text, color):
+        return f"<font color={color}>{text}</font>"
+
+    def md_add_tag(self, text, color):
+        return f"<text_tag color={color}>{text}</text_tag>"
+
+    def add_markdown(self, content=""):
+        self.data["card"]["elements"].append(self.create_markdown(content))
+
+    def add_header(self, title="", title_color="turquoise"):
+        self.data["card"]["header"] = {
+            "template": title_color,
+            "title": {"content": title + "\n", "tag": "plain_text"},
+        }
+        return
+
+    def add_series_to_table(
+        self, series, flex_mode="none", columns_background_style="default"
+    ):
+        series = series.astype(str)
+        col_list = [self.create_markdown(series[row]) for row in series.index]
+        column = self.create_column(col_list)
+        column_set_json = self.create_column_set(
+            [column], flex_mode, columns_background_style
+        )
+        self.data["card"]["elements"].append(column_set_json)
+
+    def add_dataframe_to_table(
+        self,
+        df,
+        flex_mode="none",
+        key_background_style="grey",
+        columns_background_style="default",
+    ):
+        df = df.astype(str)
+        table_header_cols = [
+            self.create_markdown(col, text_align="center") for col in df.columns
+        ]
+        # print(table_header_cols)
+        table_header_elements = [self.create_column([col]) for col in table_header_cols]
+        table_header_json = self.create_column_set(
+            table_header_elements, flex_mode, key_background_style
+        )
+
+        # table content 每行生成一个 create_column_set
+        column_set_json_list = []
+        for row in df.index:
+            cols = []
+            for col in df.columns:
+                mas = [
+                    self.create_markdown(
+                        df.loc[row, col],
+                        text_align="center",
+                    )
+                ]
+                cols.append(self.create_column(mas))
+            column_set_json = self.create_column_set(
+                cols, background_style=columns_background_style
+            )
+            column_set_json_list.append(column_set_json)
+        column_set_json_list.insert(0, table_header_json)
+        self.data["card"]["elements"].extend(column_set_json_list)
+
+    @staticmethod
+    def create_markdown(content, text_align="left"):
+        return {
+            "tag": "markdown",
+            "content": content,
+            "text_align": text_align,
+        }
+
+    @staticmethod
+    def create_column(elements, width="weighted", weight=1, vertical_align="center"):
+        return {
+            "tag": "column",
+            "width": width,
+            "weight": weight,
+            "vertical_align": vertical_align,
+            "elements": elements,
+        }
+
+    @staticmethod
+    def create_column_set(
+        columns,
+        flex_mode="none",
+        background_style="default",
+        horizontal_spacing="small",
+    ):
+        return {
+            "tag": "column_set",
+            "flex_mode": flex_mode,
+            "background_style": background_style,
+            "columns": columns,
+            "horizontal_spacing": horizontal_spacing,
+        }
+
+    # 自定义处理函数,检测是否有不支持json序列化数据
+    @staticmethod
+    def default_handler(o):
+        if isinstance(o, np.integer):
+            return int(o)
+        elif isinstance(o, np.floating):
+            return float(o)
+        elif isinstance(o, np.ndarray):
+            return o.tolist()
+        else:
+            raise TypeError(
+                f"Object of type {o.__class__.__name__} is not JSON serializable"
+            )
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # FeiShuMessageCard
 封装了飞书的消息卡片的一些方法，用于快速构建消息模板。
+
+
+# 示例
+```shell
+from FeishuCardMessage import FeishuCardMessage
+msg = FeishuCardMessage()
+msg.add_header("这是橙色标题", title_color="orange")
+# 艾特所有人
+msg.add_markdown("<at id=all></at>")
+msg.add_markdown("<text_tag color='violet'>这是彩色标签</text_tag>")
+msg.add_markdown("<font color='red'>这是彩色字体</font>")
+msg.add_markdown("[这是超链接](https://baidu.com)")
+msg.add_markdown("这是分割线\n ---\n")
+# 添加表格
+import pandas as pd
+df = pd.read_csv('/tmp/temp.csv')
+msg.add_dataframe_to_table(df)
+# 发送
+msg.message_send(YOUR_WEBHOOK)
+```
+
+![alt text](image-1.png)
